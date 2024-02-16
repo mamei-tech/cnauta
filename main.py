fa54693b2c25091a8c5c6d19cf84b30d27cb202c
kmilo <kmilo.denis.glez@yandex.com>
2024-02-15 19:52:21 -0500

    new version with fake_useragent, bs2 and requests

--- main.py
+++ main.py
@@ -1,177 +1,132 @@
-import re
-from datetime import datetime
-
 import requests
+from bs4 import BeautifulSoup
+import datetime
+import threading
+from fake_useragent import UserAgent
+
 import configparser
 
-config = configparser.ConfigParser()
+ua = UserAgent(browsers=['edge', 'chrome', 'firefox'])
 
-# current dateTime
-now = datetime.now()
-date_time_str: str = now.strftime("%Y%m%d%H%M%S%f")[:-3]
+class MainActivity:
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.config.read('config.cfg')
 
-source_hostname: str = 'secure.etecsa.net'
-source_port: str = '8443'
-source_url: str = "https://" + source_hostname + ":" + source_port
-source_url_query_servlet: str = source_url + "/EtecsaQueryServlet"
-source_url_login_servlet: str = source_url + "//LoginServlet"
-source_url_logout_servlet: str = source_url + "/LogoutServlet"
+        self.user_passw_error = -1
+        self.estado_cuenta = None
+        self.saldo_cuenta = None
+        source_hostname: str = 'secure.etecsa.net'
+        source_port: str = '8443'
+        self.source_url: str = "https://" + source_hostname + ":" + source_port
+        self.source_url_query_servlet: str = self.source_url + "/EtecsaQueryServlet"
+        self.source_url_login_servlet: str = self.source_url + "//LoginServlet"
+        self.source_url_logout_servlet: str = self.source_url + "/LogoutServlet"
+        self.csrfhw: str = ""
+        self.cookies = ""
+        self.header = {'User-Agent': str(ua.random), "Accept-Encoding": "gzip, deflate, br",}
 
-headers = {
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Host": source_hostname,
-    "Origin": source_url,
-    "Sec-Fetch-Dest": "document",
-    "Sec-Fetch-Mode": "navigate",
-    "Sec-Fetch-Site": "same-origin",
-    "Sec-Fetch-User": "?1",
-    "Sec-GPC": "1",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "Upgrade-Insecure-Requests": "1",
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) "
-                  "Chrome/109.0.0.0 Safari/537.36",
-}
+    def username(self):
+        return self.config.get('DEFAULT', "username")
 
-# Data to be written in file
-dictionary = {
-    "attr_uuid": "",
-    "logger_id": "",
-}
+    def password(self):
+        return self.config.get('DEFAULT', "password")
 
+    def logger_id(self):
+        return self.config.get('INFO', "logger_id")
 
-def begin(user: str, passwd: str) -> dict[str, str]:
-    try:
-        get_request = requests.get(source_url, headers=headers)
-    except requests.exceptions.RequestException as e:
-        print(e)
-        raise e
-    else:
-        match_wlan_user_ip = re.search(r'<input type="hidden" name="wlanuserip" id="wlanuserip" value="(.+?)"/>',
-                                       get_request.text, re.I | re.S)
-        wlan_user_ip = match_wlan_user_ip[1]
+    def attr_uuid(self):
+        return self.config.get('INFO', "attr_uuid")
 
-        match_csrfhw = re.search(r"<input type='hidden' name='CSRFHW' value='(.+?)' />", get_request.text, re.I | re.S)
-        csrfhw = match_csrfhw[1]
+    def set_attr_uuid(self, _attr_uuid):
+        self.config.set('INFO', 'attr_uuid', _attr_uuid)
+        # self.config.set('INFO', 'wlanuserip', )
 
-        return {"wlanuserip": wlan_user_ip, "wlanmac": "", "firsturl": "notFound.jsp", "ssid": "nauta_hogar", "usertype": "",
-                "gotopage": "/nauta_hogar/LoginURL/pc_login.jsp",
-                "successpage": "/nauta_hogar/OnlineURL/pc_index.jsp",
-                "loggerId": date_time_str, "lang": "es_ES", "username": user, "password": passwd,
-                "CSRFHW": csrfhw, }
+    def set_logger_id(self, _logger_id):
+        self.config.set('INFO', 'logger_id', _logger_id)
 
+    def _data(self):
+        return {
+            "wlanacname": "",
+            "wlanmac": "",
+            "firsturl": "notFound.jsp",
+            "ssid": "",
+            "usertype": "",
+            "gotopage": "/nauta_etecsa/LoginURL/mobile_login.jsp",
+            "successpage": "/nauta_etecsa/OnlineURL/mobile_index.jsp",
+            "loggerId": self.logger_id,
+            "lang": "es_ES",
+            "username": self.username(),
+            "password": self.password(),
+            "CSRFHW": self.csrfhw
+        }
 
-def query_servlet(data: dict[str, str]):
-    try:
-        # get_request = requests.get(source_url, headers=headers)
-        # post_request = requests.post(source_url_query_servlet, headers=headers, cookies=get_request.cookies, data=data)
-        post_request = requests.post(source_url_query_servlet, headers=headers, data=data)
-    except requests.exceptions.RequestException as e:
-        print(e)
-        raise e
-    else:
-        # Extract the loggerId value using regular expressions
-        logger_id = re.search(r'<input type="hidden" name="loggerId" id="loggerId" value="([^"]+)"', post_request.text)
+    def begin(self):
+        # save logger_id in config.cfg
+        self.set_logger_id(datetime.datetime.now().strftime("%Y%m%d%H%M%S%f"))
+        execute = requests.get(self.source_url, timeout=10)
+        soup = BeautifulSoup(execute.content, "html.parser")
+        # get CSRFHW
+        self.csrfhw = soup.select_one("input[name=CSRFHW]")["value"]
+        # get cookies
+        self.cookies = execute.cookies
 
-        if logger_id:
-            logger_id = logger_id.group(1)
-            print(logger_id)
+    def state(self):
+        try:
+            self.begin()
+            post = requests.post(self.source_url_query_servlet, data=self._data(),
+                                 cookies=self.cookies, allow_redirects=True, headers=self.header)
+            _soup = BeautifulSoup(post.content, "html.parser")
+
+            if "alert(\"return null\");" not in str(_soup.select("script")[-1]):
+                self.estado_cuenta = _soup.select("table#sessioninfo > tbody > tr > td")[3].text
+                self.saldo_cuenta = _soup.select("table#sessioninfo > tbody > tr > td")[1].text
+        except Exception as e:
+            print(e)
+            pass
+
+    def do_login(self):
+        try:
+            self.begin()
+            _loggin = requests.post(self.source_url_login_servlet,
+                                    data={"username": self.username(), "password": self.password()},
+                                    allow_redirects=True, timeout=10, headers=self.header)
+
+            _soup = BeautifulSoup(_loggin.content, "html.parser")
+
+            if "El nombre de usuario o contraseña son incorrectos" in _loggin.text:
+                # La contraseña es incorrecta.
+                self.user_passw_error = 1
+            elif "No se pudo autorizar al usuario" in _loggin.text:
+                # El usuario es incorrecto.
+                self.user_passw_error = 2
+            elif "Usted a realizado muchos intentos" in _loggin.text:
+                # Muchos intentos realizados
+                self.user_passw_error = 3
+            elif "Su tarjeta no tiene saldo disponible" in _loggin.text:
+                # Sin saldo en la cuenta
+                self.user_passw_error = 4
+
+            # save attr_uuid in config.cfg
+            self.set_attr_uuid(_soup.select_one("script").text.split("ATTRIBUTE_UUID=")[1].split("&")[0])
+            print(self.attr_uuid())
+        except Exception as e:
+            print(e)
+            raise e
+
+    def do_logout(self):
+        data_logout = {'username': self.username(), 'ATTRIBUTE_UUID': self.attr_uuid(), 'wlanacname': '', 'domain': '',
+                       'remove': 1, "loggerId": f"{self.logger_id()}+{self.username()}"}
+        try:
+            post_request = requests.post(self.source_url_logout_servlet, data=data_logout, timeout=10, headers=self.header)
+        except requests.exceptions.RequestException as e:
+            print(e)
+            raise e
         else:
-            logger_id = ""
-            print("loggerId not found")
-        return logger_id
+            print(post_request.text)
 
 
-def do_login(user: str, passwd: str):
-    try:
-        post_request = requests.post(source_url_login_servlet, headers=headers, data={"username": user,
-                                                                                      "password": passwd, })
-    except requests.exceptions.RequestException as e:
-        print(e)
-        raise e
-    else:
-        match_attr_uuid = re.search(r"ATTRIBUTE_UUID=(.+?)&", post_request.text, re.I | re.S)
-        attr_uuid = ""
-        result = -1
-
-        if match_attr_uuid is None:
-            match_user_login = re.search(r'"El usuario ya está conectado."', post_request.text, re.I | re.S)
-            if match_user_login is None:
-                result = 2
-            elif match_user_login.start():
-                print(match_user_login.group())
-                result = 1
-        else:
-            attr_uuid = match_attr_uuid[1]
-            print("ATTRIBUTE_UUID: ", attr_uuid)
-            result = 0
-
-        return {"attr_uuid": attr_uuid, "response": result}
-
-
-def do_logout(user: str, login_strptime: str, attr_uuid: str):
-    data_logout = {'username': user, 'ATTRIBUTE_UUID': attr_uuid, 'wlanacname': '', 'domain': '', 'remove': 1,
-                   "loggerId": f"{login_strptime}+{user}"}
-
-    print("---------------------begin: LogoutServlet--------------------")
-    try:
-        post_request = requests.post(source_url_logout_servlet, data=data_logout)
-    except requests.exceptions.RequestException as e:
-        print(e)
-        raise e
-    else:
-        print(post_request.text)
-        print("----------------------end: LogoutServlet-------------------")
-
-
-def sign_in(user: str, passwd: str):
-    """
-    To log in with the Nauta portal to use this function
-    :param user:
-    :param passwd:
-    :return:
-    """
-    try:
-        data = begin(user, passwd)
-    except Exception as e:
-        raise Exception(e)
-    else:
-        attr_uuid = do_login(user, passwd)
-        # If the response key is 0 satisfactory, but it is 1 then the user is already authenticated
-        if attr_uuid.get('response', 1) == 0:
-            # get loggerId
-            logger_id = query_servlet(data)
-
-            config.set('INFO', 'attr_uuid', attr_uuid.get('attr_uuid', ''))
-            config.set('INFO', 'logger_id', logger_id)
-            config.set('INFO', 'wlanuserip', data.get('wlanuserip', ''))
-
-            # writing the necessary info to the file to use later in the logout
-            with open('config.cfg', 'w') as configfile:
-                config.write(configfile)
-            print("Login")
-
-
-def sign_out(user: str):
-    """
-    To logout with the Nauta portal to use this function
-    :param passwd:
-    :param user:
-    :return:
-    """
-    config.read('config.cfg')
-    do_logout(user, config.get('INFO', "logger_id"), config.get('INFO', "attr_uuid"))
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    config.read('config.cfg')
-    username = config.get('DEFAULT', "username")
-    password = config.get('DEFAULT', "password")
-
-    if username != "" and password != "":
-        sign_in(username, password)
-
-    # if username != "":
-    #     sign_out(username)
+    ma = MainActivity()
+    threading.Thread(target=ma.do_login()).start()
+    threading.Thread(target=ma.do_logout()).start()
